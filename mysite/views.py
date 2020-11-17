15661fbfcdf77ce5e955ab07b69a657633fab58a
jane-8239 <1061238239@nkust.edu.com>
2020-11-17 12:02:38 +0800

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from mysite.models import Post
+import random
+
+def homepage(request):
+    posts = Post.objects.all()
+    return render(request, "index.html", locals())
+     
+def lotto(request):
+    lucky =random.randint(1 ,42)
+    lottos = list()
+    for i in range(6):
+        lottos.append(random.randint(1 ,42))
+
+    return render(request, "lotto.html", locals())
+    
+
